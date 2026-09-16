afcd5d638900e1f659b723b04373bac55596387c
Azziiieee <balendez.alodiazenny26@gmail.com>
2026-09-16 15:19:28 +0800

    Updated indexes

--- index.php
+++ index.php
@@ -4,44 +4,58 @@ require_once 'config/functions.php';
 
 if(isset($_SESSION['user_id'])){
     header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
+    exit;
 }
 
 $error='';
 
-if($_SERVER['REQUEST_METHOD'] === ''){
-    $login =trim($_POST['login'] ?? '');
-    $password = $_POST['password'] ?? '';
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $login = trim($_POST['login'] ?? '');
+    $password =$_POST['password'] ?? '';
 
-    if(loginUser($pdo,$login,$password)){
-        header('location:' . BASE_URL . '/app' . $_SESSION['user_role'] . '/index.php');
+ $error = 'Invalid login credentials';
+
+    if ($login=== '' || $password === ''){
+        //Log Incomplete
+        logActivity($pdo,null,$login,'login','failed');
+    }else{
+        
+        if(loginUser($pdo,$login,$password)){
+            //Log complete login attempt
+            logActivity($pdo,$_SESSION['user_id'],$_SESSION['user_email'],'login','success');
+            header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
+
+        }
     }
+
 }
+
+   
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF=8">
-    <meta name="veiwport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
-
+    
 <form method="POST">
     <label>Username or Email</label>
     <input type="text"
-                name="login"
-                required>
-        <br>
-        <br>
-        <label>Password</label>
-        <input type="password"
-                name="password"
-                required>
-        <br>
-        <button type="submit">Sign In</button>
-    </form>
-  
-    </body>
-    </html>
+            name="login"
+            required>
+    <br>
+    <br>
+    <label>Password</label>
+    <input type="password"
+            name="password"
+            required>
+    <br>
+    <button type="submit">Sign In</button>
+    
+</body>
+</html>
